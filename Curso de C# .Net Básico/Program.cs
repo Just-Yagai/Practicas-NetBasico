a41b272d370a6c523b542c6c84b60a98e30e82db
Sirius <romerperez26@gmail.com>
2024-04-27 16:29:35 -0400

    Practica 1 #Comment Realizando la practica 1: programación secuencial.

--- Curso de C# .Net Básico/Program.cs
+++ Curso de C# .Net Básico/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Curso_de_C__.Net_Básico.Class;
+
+Calculadora calculadora = new Calculadora();
+
+calculadora.CalculadoraProducto();
